39a3b0b3d4cf41b890a14e0b6c1daf5c12a73dbc
Rasmus Andersson <rasmus@notion.se>
2010-03-24 20:33:20 +0100

    whitespace

--- client/lib/oui/pipe/transport/websocket.js
+++ client/lib/oui/pipe/transport/websocket.js
@@ -1,7 +1,7 @@
 // WebSocket -- http://dev.w3.org/html5/websockets/
 
 exports.isUseable = function(){
-	return oui.capabilities.webSocket;
+  return oui.capabilities.webSocket;
 };
 
 exports.Transport = function(options) {
@@ -10,13 +10,13 @@ exports.Transport = function(options) {
 
 oui.mixin(exports.Transport.prototype, __parent.Transport.prototype, {
 
-	connect: function(callback) {
+  connect: function(callback) {
     var self = this;
-	  if (this.socket) {
-	    var rs = this.socket.readyState;
-	    if (rs === WebSocket.OPEN) {
-	      if (callback) callback();
-	      return false;
+    if (this.socket) {
+      var rs = this.socket.readyState;
+      if (rs === WebSocket.OPEN) {
+        if (callback) callback();
+        return false;
       }
       else if (rs === WebSocket.CONNECTING) {
         if (callback) this._addConnectCallback(callback);
@@ -30,54 +30,54 @@ oui.mixin(exports.Transport.prototype, __parent.Transport.prototype, {
         return false;
       }
       // else it's CLOSED and we can crete a new connection
-	  }
-		this.socket = new WebSocket(this._mkURL());
-		this.socket.onopen = function(ev){ self.emit('open'); };
-		this.socket.onmessage = function(ev){ self._handleMessage(ev.data); };
-		this.socket.onerror = function(ev, err){ self.emit.apply(self, 'error', err); };
-		this.socket.onclose = function(ev){ self.emit('close', ev.wasClean); };
-		if (callback) this._addConnectCallback(callback);
-		return true;
-	},
+    }
+    this.socket = new WebSocket(this._mkURL());
+    this.socket.onopen = function(ev){ self.emit('open'); };
+    this.socket.onmessage = function(ev){ self._handleMessage(ev.data); };
+    this.socket.onerror = function(ev, err){ self.emit.apply(self, 'error', err); };
+    this.socket.onclose = function(ev){ self.emit('close', ev.wasClean); };
+    if (callback) this._addConnectCallback(callback);
+    return true;
+  },
 
-	send: function(data){
-		var sentOrQueued = this.socket.send(this._formatMessage(data));
-		return sentOrQueued;
-	},
+  send: function(data){
+    var sentOrQueued = this.socket.send(this._formatMessage(data));
+    return sentOrQueued;
+  },
 
-	disconnect: function(callback){
-	  if (this.socket && this.socket.readyState === WebSocket.CLOSED)
-	    return callback && callback();
-		this.socket.close();
-		if (callback) this.on('close', true, function(){ callback(); });
-	},
+  disconnect: function(callback){
+    if (this.socket && this.socket.readyState === WebSocket.CLOSED)
+      return callback && callback();
+    this.socket.close();
+    if (callback) this.on('close', true, function(){ callback(); });
+  },
 
   _mkURL: function() {
     var backend = this.backend();
-  	return (backend.secure ? 'wss' : 'ws')+
-  	  '://' + backend.host + ':' + backend.port+
-  	  this.options.path+ '/' + encodeURIComponent(this.options.channel)+
-  	  '/websocket';
+    return (backend.secure ? 'wss' : 'ws')+
+      '://' + backend.host + ':' + backend.port + (backend.path || '')
+      this.options.path+ '/' + encodeURIComponent(this.options.channel)+
+      '/websocket';
   },
   
   _addConnectCallback: function(callback) {
     var self = this;
-	  var onopen = function(){ cleanup(); callback(); };
-	  var onerr = function(ev, err){
-	    cleanup(); callback(err || new Error('unspecified WebSocket error'));
-	  };
-	  // some browsers do not emit error, but instead a close w/o any error info
-	  var onclose = function(ev){
-	    cleanup();
-	    var e = new Error('unspecified WebSocket connection error');
-	    e.event = ev;
-	    callback(e);
-	    self.emit('error', e);
-	  };
-	  var socketq = $(this.socket);
-	  var cleanup = function() {
-	    socketq.unbind('open',onopen).unbind('error',onerr).unbind('close',onclose);
-	  }
-	  socketq.one('open', onopen).one('error', onerr).one('close', onclose);
+    var onopen = function(){ cleanup(); callback(); };
+    var onerr = function(ev, err){
+      cleanup(); callback(err || new Error('unspecified WebSocket error'));
+    };
+    // some browsers do not emit error, but instead a close w/o any error info
+    var onclose = function(ev){
+      cleanup();
+      var e = new Error('unspecified WebSocket connection error');
+      e.event = ev;
+      callback(e);
+      self.emit('error', e);
+    };
+    var socketq = $(this.socket);
+    var cleanup = function() {
+      socketq.unbind('open',onopen).unbind('error',onerr).unbind('close',onclose);
+    }
+    socketq.one('open', onopen).one('error', onerr).one('close', onclose);
   }
 });
